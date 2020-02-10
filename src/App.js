7f7456be90a820c73980f4d09a38826a26786b96
Nicolas Cardenas <nicocrash97@hotmail.com>
2020-02-10 16:54:15 -0500

    react app running, first html view test, bootstrap grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col xs={12} sm={3} md={2} lg={1}><di>MOMS KITCHEN</di></Col>
+          <Col xs={12} sm={3} md={2} lg={1}>.</Col>
+          <Col xs={12} sm={3} md={2} lg={1}>............................................</Col>
+          <Col xs={12} sm={3} md={2} lg={1}>..........................................................................</Col>
+        </Row>
+      </Container>      
     </div>
   );
 }
