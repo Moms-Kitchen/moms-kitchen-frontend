35b0a38960cbf65af1b0306673e2dae1432a2a9f
Nicolas Cardenas <nicocrash97@hotmail.com>
2020-05-21 05:25:54 -0500

    signup view

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 //import LoginTemp from './views/Login'
 import Login from './views/Login'
 import Kitchen from './views/Kitchen'
+import Signup from './views/Signup'
 
 import Billing from './components/Billing'
 import Orders from './components/Orders'
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Login}></Route>
         <Route exact path='/login' component={Login}></Route>
+        <Route exact path='/signup' component={Signup}></Route>
         <Route exact path='/Kitchen' component={Kitchen}></Route>
 
         <Route exact path='/Billing' component={Billing}></Route>
